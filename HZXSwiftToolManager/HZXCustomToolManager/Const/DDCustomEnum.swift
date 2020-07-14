de406049c96f0c358891919c1efde0829278ff4b
侯仲祥 <1418021044@qq.com>
2020-07-14 17:41:53 +0800

    增加自定义枚举文件
    1.手机验证码类型枚举
    2.订单状态枚举

---
+++ HZXSwiftToolManager/HZXCustomToolManager/Const/DDCustomEnum.swift
@@ -0,0 +1,38 @@
+//
+//  DDCustomEnum.swift
+//  HZXSwiftToolManager
+//
+//  Created by 侯仲祥 on 2020/7/14.
+//  Copyright © 2020 houZhongXiang. All rights reserved.
+//
+
+import Foundation
+
+/// 手机短信验证码类型
+public enum DDPhoneCodeType: Int {
+    /// 注册
+    case register = 0
+    /// 忘记密码
+    case forggetPwd
+    /// 修改密码
+    case changePwd
+}
+
+/// 订单状态
+public enum DDOrderStauts: Int {
+    /// 全部订单
+    case all = 0
+    /// 待支付
+    case daiZhiFu
+    /// 代发货
+    case daiFaHuo
+    /// 待收货
+    case daiShouHuo
+    /// 待评价
+    case daiPingJia
+    /// 已完成
+    case completed
+    /// 已取消
+    case canceled
+    
+}
